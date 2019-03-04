d66426d62c18f03a32dcb9126daacbc00f5b369f
Ilijan Kotarac <ilijan.kotarac@fiveminutes.eu>
2019-03-04 02:21:16 +0100

    Fixed export.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ export {
   createScope as default,
   createScope,
   createScope as scope,
-} from './scope';
+} from './create-scope';
 export { createRootReducer } from './create-root-reducer';
 export { combineSelectors } from 'redux-scope-helpers';
