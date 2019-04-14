a922829e24f7c6e8c3bebf6d2fddf2f0d2e8d5d6
Ilijan Kotarac <ilijan.kotarac@fiveminutes.eu>
2019-04-14 21:45:08 +0200

    Removed redux-scope-helpers dependency. Exported withScope.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export { createAction, createThunk } from './actions';
 export { createScope, combineScopes } from './scopes';
 export { createSelector, createSelectors, combineSelectors } from './selectors';
+export { withScope } from './with-scope';
 export { createRootReducer } from './reducer';
 
 export default createScope;
